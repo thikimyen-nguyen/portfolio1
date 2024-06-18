4fc013b61a2e97582f1054cc4e5206c8c84e1f60
Yen Nguyen <nguyenthikimyen25@gmail.com>
2024-06-18 20:24:51 +0200

    add new project

--- src/js/data.mjs
+++ src/js/data.mjs
@@ -1,8 +1,16 @@
 export const projects = [
   {
-    "name": "Holidaze Accommodation Site",
+    "name": "Tista Sushi in Halden",
+    "image": "images/tistasushi.png",
+    "description": "This project is for a real client, Tista Sushi restaurant in Halden.  I am currently working on this and plan to deliver the site at the end of Jun 2024",
+    "type": "Real Project (on-going)",
+    "githubLink": "https://github.com/thikimyen-nguyen/tista-sushi",
+    "liveSite": "https://tistasushi.netlify.app/"
+  },
+  {
+    "name": "Holidaze Booking Site",
     "image": "images/holidaze.webp",
-    "description": "Final project for an accommodation booking application, which built with React and Tailwind to create booking functionality.",
+    "description": "Final school project for an accommodation booking application, which built with React and Tailwind to create booking functionality.",
     "type": "Project Exam 2",
     "githubLink": "https://github.com/thikimyen-nguyen/project-exam-2",
     "liveSite": "https://holidaze-no.netlify.app/"
@@ -10,7 +18,7 @@ export const projects = [
   {
     "name": "B.Shop Online Site",
     "image": "images/bshop.png",
-    "description": "An current school project for an online shop, which focuses on React, Tailwind CSS and uses Noroff API to create a functional shopping cart.",
+    "description": "A school project for an online shop, which focuses on React, Tailwind CSS and uses Noroff API to create a functional shopping cart.",
     "type": "JavaScript React Project",
     "githubLink": "https://github.com/thikimyen-nguyen/js-framework-react",
     "liveSite": "https://bshop-no.netlify.app/"
@@ -18,7 +26,7 @@ export const projects = [
   {
     "name": "babeebids Auction Site",
     "image": "images/babeebids.webp",
-    "description": "An auction site for baby and maternity products. This project in the 2nd year at Noroff focused on CSS framework and more in JavaScript",
+    "description": "An auction site for baby and maternity products. This project was focused on CSS framework and more in JavaScript using Noroff API",
     "type": "Semester Project 2",
     "githubLink": "https://github.com/thikimyen-nguyen/semester-project2-babybids",
     "liveSite": "https://babeebids.netlify.app/"
